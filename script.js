4c7f535724be8b2356daf579088d241a7c6d23dc
Binu Tomy <BinuTomy@Binus-MacBook-Pro.local>
2017-11-08 21:36:13 +0000

    fixing function and adding styles

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
+/**
+ * Populating Table of contents:
+ * TOC will be added if the advisor is logged in
+ *
+ * Author: @Citizens Advice
+ */
+
 'use strict';
 
-$(function () {
+var TableOfContents = (function () {
     
     var tocItems = $('h2.js-ref'); 
-    var advisorLogin = $("input[name*='pageRatingYes']"); 
+    var adviceContent = document.getElementsByClassName("articleContent");
+    var advisorLogin = ($("input[name*='radio-inline']").val()); 
     var tocArray = [];
+    var tocList;
 
-    var testArray =[];
-
-    var tocContents = {
-        'id' : '',
-        'text': ''
-    };
-
-    if (advisorLogin) {
-        createTitlteList(tocItems);
-        returnArrayItems(tocArray);
-        console.log("");
-    }
-
-
-    function createTitlteList(item) {
-        tocArray.push(item);
-        return tocArray;
-    };
-
-
-    function returnArrayItems(array) {
-
-        array.forEach(function(options) {
-            
-            for(var i = 0; i < options.length; i++)
-            {  
-             tocContents.id = options[i].id ;
-             tocContents.text = options[i].innerText ;         
-            }
-            testArray.push(tocContents)
-
+    function addingToTOC(options) {
+        $(options).each(function() {
+            var tempObj = { 
+                id: this.id, 
+                content: this.textContent 
+            };
+            tocArray.push(tempObj);
         });
-        return testArray;
+    };
+
+    function populateTOC() {
+        var tempItems = '';
+        addingToTOC(tocItems);
+        tocArray.forEach(function(item){
+            tempItems += "<li><a href=#" + item.id + ">" + item.content + "</a></li>";
+        });
+        return tocList = ("<div class='well TOC'><span> On this page </span><ul> " + tempItems + " </ul></div>");
+    };
+
+    function renderList() {
+        if (advisorLogin === "yes" ) {
+            populateTOC();
+            $(adviceContent).prepend(tocList);
+        }
     }
 
-}());
+    return{
+        render: renderList
+    }
+
+})();
+
+TableOfContents.render(); //This will return a list of all the Titles in a Div
