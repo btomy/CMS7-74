b8572fc48559fdc9e3e3cc446d8d4c67fc4ba993
Binu Tomy <BinuTomy@binus-mbp.cabsrv.org.uk>
2017-11-16 10:10:33 +0000

    updating code addding doc ready

--- script.js
+++ script.js
@@ -19,53 +19,58 @@
 
 'use strict';
 
-var TableOfContents = (function () {
-    
-    var tocItems = $('.ref-heading__heading'); 
-    var tocItemsID = $('.ref-heading');
-    var adviceContent = document.getElementsByClassName("articleContent");
-    var advisorLogin = ($('input[name*="astat"]').val());
-    var tocArray = [];
-    var tocList;
+$( document ).ready(function() {
 
-    function addingToTOC(text,id) {
-        $.each(text, function (i, item) {
-             var tempObj = { 
-                id: id[i].id, 
-                content: item.textContent
-            };
-            tocArray.push(tempObj);
-		});
-    };
+    var TableOfContents = (function () {
+        
+        var tocItems = $('.ref-heading__heading'); 
+        var tocItemsID = $('.ref-heading');
+        var adviceContent = document.getElementsByClassName("articleContent");
+        var advisorLogin = ($('input[name*="astat"]').val());
+        var tocArray = [];
+        var tocList;
 
-    function populateTOC() {
-        var tempItems = '';
-        addingToTOC(tocItems,tocItemsID);
-        tocArray.shift(); //removing the first element in the array. As this is the title of the page
-        tocArray.forEach(function(item){
-            tempItems += '<li><a href=#' + item.id + '>' + item.content + '</a></li>';
-        });
-        return tocList = (
-            '<div class="well tertiary">'+
-                '<h5> On this page </h5>'+
-                '<ul class="ul--2-column">'+
-                    tempItems+
-                '</ul>'+
-            '</div>'
-        );
-    };
+        function addingToTOC(text,id) {
+            $.each(text, function (i, item) {
+                 var tempObj = { 
+                    id: id[i].id, 
+                    content: item.textContent
+                };
+                tocArray.push(tempObj);
+    		});
+        };
 
-    function renderList() {
-        if (advisorLogin === 'True' ) {
-            populateTOC();
-            $(adviceContent).prepend(tocList);
+        function populateTOC() {
+            var tempItems = '';
+            addingToTOC(tocItems,tocItemsID);
+            tocArray.shift(); //removing the first element in the array. As this is the title of the page
+            tocArray.forEach(function(item){
+                tempItems += '<li><a href=#' + item.id + '>' + item.content + '</a></li>';
+            });
+            return tocList = (
+                '<div class="well tertiary">'+
+                    '<h5> On this page </h5>'+
+                    '<ul class="ul--2-column">'+
+                        tempItems+
+                    '</ul>'+
+                '</div>'
+            );
+        };
+
+        function renderList() {
+            if (advisorLogin === 'True' ) {
+                populateTOC();
+                $(adviceContent).prepend(tocList);
+            }
         }
-    }
 
-    return{
-        render: renderList
-    }
+        return{
+            render: renderList
+        }
 
-})();
+    })();
 
-TableOfContents.render(); //This will return a list of all the Titles in a Div
+
+    TableOfContents.render(); //This will return a list of all the Titles in a Div
+});
+
